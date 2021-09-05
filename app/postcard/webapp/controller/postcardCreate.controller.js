63de9c2df47d839826d628a2b65d4e4972f4b1fd
Divye Aggarwal <divye.aggarwal@sap.com>
2021-09-05 09:34:25 +0000

    Fixing chnages list page

--- app/postcard/webapp/controller/postcardCreate.controller.js
+++ app/postcard/webapp/controller/postcardCreate.controller.js
@@ -10,6 +10,7 @@ sap.ui.define([
 
 		return Controller.extend("com.sap.postcard.postcard.controller.postcardCreate", {
 			onInit: function () {
+                this.getOwnerComponent().getRouter().getRoute("productDetails").attachPatternMatched(this._onRouteMatched, this);
 
             //     var oModel = new sap.ui.model.odata.v2.ODataModel("/v2/catalog/", {
 			// 	defaultBindingMode: sap.ui.model.BindingMode.TwoWay,
@@ -17,9 +18,21 @@ sap.ui.define([
 			// });
             // this.getView().setModel(oModel);
             // // oModel.read("PostCard(ID='738b04e1-3f20-46d2-a536-2794a56169a4')");
-            var oModel = new JSONModel(sap.ui.require.toUrl("com/sap/postcard/postcard/model/data.json"));
-			this.getView().setModel(oModel)
+            // var oModel1 = new JSONModel(sap.ui.require.toUrl("com/sap/postcard/postcard/model/data.json"));
+			// this.getView().setModel(oModel1)
 
-			}
+            },
+            
+            //navigate to SR Page
+		onAddPostcard: function () {
+			//Ensure solution Results record is presen tin backend before navigating to the page
+            // var that1 = this;
+            this.getRouter()
+			this.getOwnerComponent().getRouter().navTo("RoutepostcardCreate", {
+							// solRequest: this.SRnumber,
+							// mode: "Display"
+							// SRNAME: solreqname
+						});
+		},
 		});
 	});
